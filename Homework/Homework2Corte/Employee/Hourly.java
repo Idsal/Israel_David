34cf0a72fe3b68622880d5eee64b6cbaecced393
Idsal <35944819+Idsal@users.noreply.github.com>
2018-04-16 21:04:48 -0500

    Add files via upload

---
+++ Homework/Homework2Corte/Employee/Hourly.java
@@ -0,0 +1,25 @@
+/*
+Hourly.java
+Israel David Salinas Lopez
+**/
+public class Hourly extends Employee
+{
+  private double HourlyRate=150;
+  private double hours=0.0;
+
+  public Hourly(String name, double hours)
+  {
+    super(name);
+    this.hours=hours;
+  }
+  @Override
+  public  double getPay()
+  {
+    double pay=HourlyRate*hours;
+    return pay;
+  }
+  public void addHours(double hours)
+  {
+    this.hours+=hours;
+  }
+}
